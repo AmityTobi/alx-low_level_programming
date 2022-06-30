116af793e87949e25bac3ed5958656602ca6e32e
AmityTobi <amityekoyi@gmail.com>
2022-06-30 09:45:27 -0700

    gettig sizes of datatype

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,7 @@
  *
  * printf - Used to print formatted text
  *
- * Retun: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -16,11 +16,10 @@ int main(void)
 	int float_type = sizeof(float);
 	long long_type = sizeof(long);
 	long ll_type =sizeof(long long);
-		
-	printf("Size of a char: %d byte(s)\n",ch);
-	printf("Size of an int: %d byte(s)\n",int_type);
-	printf("Size of a long int: %li byte(s)\n",long_type);
-	printf("Size of a long long int: %ld byte(s)\n",ll_type);
-	printf("Size of a float: %d byte(s)\n",float_type);
-	return (0);
+		printf("Size of a char: %d byte(s)\n",ch);
+		printf("Size of an int: %d byte(s)\n",int_type);
+		printf("Size of a long int: %li byte(s)\n",long_type);
+		printf("Size of a long long int: %ld byte(s)\n",ll_type);
+		printf("Size of a float: %d byte(s)\n",float_type);
+		return (0);
 }
