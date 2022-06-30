e46d7e1d32f41e49e931e49342a70229255ca7e1
AmityTobi <amityekoyi@gmail.com>
2022-06-30 07:51:31 -0700

    using the sizeof function

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * sizeof - Used to print type sizes
+ *
+ * printf - Used to print formatted text
+ *
+ * Retun: Always 0 (Success)
+ */
+int main(void)
+{
+	ch = sizeof(char);
+	int_type = sizeof(int);
+	float_type = sizeof(float);
+	double_type = size0f(double);
+	return (0);
+}
