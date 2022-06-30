a1a8bfff7524a27be2d21635905c64d58c80cfc7
AmityTobi <amityekoyi@gmail.com>
2022-06-30 08:41:30 -0700

    Getting sizes of types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,5 +14,13 @@ int main(void)
 	char ch = sizeof(char);
 	int int_type = sizeof(int);
 	float float_type = sizeof(float);
+	long long_type = sizeof(long);
+	long ll_type =sizeof(long long);
+		
+	printf("size of a char: %c byte(s)\n",ch);
+	printf("size of an int: %d byte(s)\n",int_type);
+	printf("size of a long int: %li byte(s)\n",long_type);
+	printf("size of a long long int: %ld byte(s)\n",long_type);
+	printf("size of a float: %f byte(s)\n",float_type);
 	return (0);
 }
