4d42933070413f3f09dcb4f85abf6bd0892e262d
AmityTobi <amityekoyi@gmail.com>
2022-06-30 06:34:15 -0700

    using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+
 /**
  * Using the puts
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
