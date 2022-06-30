bcdc675207097803dbce8d6257343d05d0857b48
AmityTobi <amityekoyi@gmail.com>
2022-06-30 07:38:17 -0700

    using the printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * printf - Using the printFormat
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	
+	return (0);
+}
