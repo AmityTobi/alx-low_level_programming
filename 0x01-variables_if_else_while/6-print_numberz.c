144ae29cc0bd4fbd40af0ca6f43d7a5158414a04
AmityTobi <amityekoyi@gmail.com>
2022-07-01 16:49:23 -0700

    printing numbers using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print 0123456789 using putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = '0';
+
+	while (i <='9')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
