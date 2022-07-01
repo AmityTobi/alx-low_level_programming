f3f2b931d77fb1b7ecb39de2941e612f33a251cb
AmityTobi <amityekoyi@gmail.com>
2022-07-01 16:24:56 -0700

    printing digit numbers of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return - 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+		printf("%i\n", i);
+		return (0);
+}
