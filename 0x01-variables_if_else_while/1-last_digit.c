cef2107c2d2c762745b73c28e27aa4f43769b84b
AmityTobi <amityekoyi@gmail.com>
2022-07-01 07:03:34 -0700

    This program will assign a random number to the variable n each time it is executed as well as print the last line

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -27,7 +27,6 @@ int main(void)
                 printf("Last digit of %d is %d and is 0\n ", n, last_digit);                
         } else if ((last_digit < 6) && (last_digit != 0))                                   
         {                                                                                   
-                printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digi
-t);                                                                                         
+                printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);                                                                                       
         }                                                                                   
 }                                                                                           
