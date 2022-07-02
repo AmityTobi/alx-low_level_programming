1cb35d22ac96f3947bc11da9b81511f2310c37f5
AmityTobi <amityekoyi@gmail.com>
2022-07-01 18:54:45 -0700

    combinations of three different numbers

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -21,7 +21,7 @@ int main(void)
 				putchar(i);
 				putchar(j);
 				putchar(k);
-				if (i != '8' || j != '9')
+				if (i != '7' || j != '8' || k != '9')
 				{
 					putchar(',');
 					putchar(' ');
