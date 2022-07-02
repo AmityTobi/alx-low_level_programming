7340b12373211bc9c0a2e5e0018944d0404d8db3
AmityTobi <amityekoyi@gmail.com>
2022-07-01 18:47:06 -0700

    combinations of three different numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i; /* represents hundreds*/
+	int j; /* represents tens*/
+	int k; /* represents units*/
+
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = (i + 1); j <= '9'; j++)
+		{
+			for (k = (j + 1); k <= 9; k++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+
+				if (i != '8' || j != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+
+	}
+	putchar('\n');
+	return (0);
+}
