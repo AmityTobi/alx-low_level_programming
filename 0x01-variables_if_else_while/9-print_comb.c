80f4e932e717e24686b513f988cfde8592eebfef
AmityTobi <amityekoyi@gmail.com>
2022-07-01 17:24:11 -0700

    adding commas to printed numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print 0 through to 9 seperetad by commas, using putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(',');
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
