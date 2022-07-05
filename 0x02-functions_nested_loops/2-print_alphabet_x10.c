abed48f1cfe5378e6a864c4e7f2b7bca638b527c
AmityTobi <amityekoyi@gmail.com>
2022-07-05 14:22:30 -0700

    prints 10 times the alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabets in lowercase 10 times
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		i = 0;
+		
+		while (i < 9)
+		{
+			_putchar(ch);
+			i++;
+		}
+	}
+	_putchar('\n');
+	ch++;
+	return (0);
+}
