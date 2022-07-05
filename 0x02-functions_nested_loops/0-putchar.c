57963ce9442d5a868b2b73251dc0d7e9fca528be
AmityTobi <amityekoyi@gmail.com>
2022-07-05 08:18:28 -0700

    functions,nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char ch[] = "_putchar";
+	int i = 0;
+
+	while (i < 8)
+	{
+		_putchar(ch[i]);
+		i++;
+	}
+	_putchar("\n");
+	return (0);
+}
